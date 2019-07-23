81e1e93c163e1f5c88aba77d0017707069e0c29b
gonpeche <espeche.g@gmail.com>
2019-07-23 15:55:20 -0300

    canvas working

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+  // faceapi.nets.extractFaces.loadFromUri('/models'),
 ]).then(startVideo)
 
-console.log(faceapi)
 function startVideo() {
   navigator.getUserMedia(
     { video: {} },
@@ -15,3 +15,17 @@ function startVideo() {
     err => console.error(err)
   )
 }
+
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
+  const displaySize = { width: video.width, height: video.height }
+  faceapi.matchDimensions(canvas, displaySize)
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+  }, 100)
+})
