8d9493bf12d621ed9f41ed1cfddaa60708c592d3
gonpeche <espeche.g@gmail.com>
2019-07-23 15:01:54 -0300

    added live server

---
+++ script.js
@@ -0,0 +1,17 @@
+const video = document.getElementById('video')
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo)
+
+console.log(faceapi)
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
+}
